ec7e943a3d17de6b8e59c8fcb931f1f97171a741
Findus23 <l.winkler23@me.com>
2014-12-23 18:44:47 +0100

    Zahlen mit führenden Nullen

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ convert -monitor -resize 100x Logo-ohne-BRG-klein.png ./done/logo.png
 a=1
 for i in *.JPG
 do
+	a_f="$(printf "%02d" $a)"
 	echo "./neu/Bild_$a.jpg"
 	convert -monitor -caption '%f' -resize 640x $i  \
 		-gravity SouthEast ./done/logo.png -geometry +15+15 \
@@ -19,7 +20,7 @@ do
 			-bordercolor snow -background black \
 			 -gravity center -font 'Liberation Sans' -pointsize 26 \
 		+polaroid \
-		./done/2008-­Sanierung-­$a.png
+		./done/2008-­Sanierung-­$a_f.png
 	((a++))
 done
 montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.png -caption '%f\n%wx%h' -font 'Liberation Sans' -pointsize 20 -background transparent -frame 5 ./done/test.PNG
