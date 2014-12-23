1a396c3f1b865ed6f54cc1936a1ccc110f6a5fe5
Findus23 <l.winkler23@me.com>
2014-12-23 19:03:11 +0100

    Zenity-Fragen hinzugefügt

--- script.sh
+++ script.sh
@@ -8,7 +8,21 @@ else
 	rm ./done/*
 	echo "alte Dateien gelöscht"
 fi
+
+format=$(zenity --entry --text "Welches Dateiformat soll ausgegeben werden?" --title "Dateiformat") || exit 1
+if [ -z $format ]
+then
+	format="png"
+fi
+
+name=$(zenity --entry --text "Welchen  Namen sollen die Dateien haben?" --title "Dateiname") || exit 1
+if [ -z $name ]
+then
+	name="2008­-Sanierung­-"
+fi
+
 convert -monitor -resize 100x Logo-ohne-BRG-klein.png ./done/logo.png
+
 a=1
 for i in *.JPG
 do
@@ -20,7 +34,9 @@ do
 			-bordercolor snow -background black \
 			 -gravity center -font 'Liberation Sans' -pointsize 26 \
 		+polaroid \
-		./done/2008-­Sanierung-­$a_f.png
+		./done/$name­$a_f.$format
 	((a++))
 done
-montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.png -caption '%f\n%wx%h' -font 'Liberation Sans' -pointsize 20 -background transparent -frame 5 ./done/test.PNG
+montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.$format -caption '%f\n%wx%h' -font 'Liberation Sans' -pointsize 20 -background transparent -frame 5 ./done/Thumbnails.$format
+rm ./done/logo.png
+zenity --info --text "Das Programm ist fertig." --title "Fertig"
