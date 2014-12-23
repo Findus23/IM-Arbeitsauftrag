fb6adf7c84e39c2e3abe3718536a6936c5f71a1b
Findus23 <l.winkler23@me.com>
2014-12-23 18:22:05 +0100

    Wasserzeichen + Polaroid

--- script.sh
+++ script.sh
@@ -8,11 +8,17 @@ else
 	rm ./done/*
 	echo "alte Dateien gelöscht"
 fi
-
+convert -monitor -resize 100x Logo-ohne-BRG-klein.png ./done/logo.png
 a=1
 for i in *.JPG
 do
 	echo "./neu/Bild_$a.jpg"
-	convert -monitor -resize 640x640 $i ./done/2008-­Sanierung-­$a.png
+	convert -monitor -caption '%f' -resize 640x $i  \
+		-gravity SouthEast ./done/logo.png -geometry +15+15 \
+		-composite \
+			-bordercolor snow -background black \
+			 -gravity center -font 'Liberation Sans' -pointsize 26 \
+		+polaroid \
+		./done/2008-­Sanierung-­$a.png
 	((a++))
 done
