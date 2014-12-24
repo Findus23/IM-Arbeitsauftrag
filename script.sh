e8868b42d24ba42793f79309f522de236453d4fe
Findus23 <l.winkler23@me.com>
2014-12-24 12:12:19 +0100

    Übersetzng in Deutsch, Englisch und Französisch

--- script.sh
+++ script.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+TEXTDOMAIN=script
+TEXTDOMAINDIR=./
+
 if [ ! -d done ]
 then
 	mkdir ./done
-	echo "Neuen Ordner erstellt"
+	echo $"created new folder"
 else
 	rm ./done/*
-	echo "alte Dateien gelöscht"
+	echo $"deleted old files"
 fi
 
-format=$(zenity --entry --text "Welches Dateiformat soll ausgegeben werden?" --title "Dateiformat") || exit 1
+format=$(zenity --entry --text $"What is the export file format?" --title $"file format") || exit 1
 if [ -z $format ]
 then
 	format="png"
 fi
 
-name=$(zenity --entry --text "Welchen  Namen sollen die Dateien haben?" --title "Dateiname") || exit 1
+name=$(zenity --entry --text $"How should the files be named?" --title $"file name") || exit 1
 if [ -z $name ]
 then
 	name="2008­-Sanierung­-"
@@ -24,14 +27,14 @@ fi
 convert -monitor -resize 100x Logo-ohne-BRG-klein.png ./done/logo.png
 
 a=1
-exec 3> >(zenity --progress --title="Konvertierung..." --percentage=0 --width=400)
-echo "# Ermittle Dateien..." >&3
+exec 3> >(zenity --progress --title=$"converting..." --percentage=0 --width=400)
+echo $"# determining files..." >&3
 dateien=(*.JPG)
 dateianzahl=${#dateien[@]}
 for i in ${dateien[@]}
 do
 
-	echo "# Datei '$i' wird konvertiert... ($a von $dateianzahl)" >&3;
+	echo $"# converting '$i'... ($a out of $dateianzahl)" >&3;
 	a_f="$(printf "%02d" $a)" # Darstellung mit führender Null
 	convert -monitor -caption '%f' -resize 640x $i  \
 		-gravity SouthEast ./done/logo.png -geometry +15+15 \
@@ -43,8 +46,8 @@ do
 	echo "$a/$dateianzahl*100" | bc -l >&3
 	((a++))
 done
-echo "# Thumbnails werden erstellt..." >&3
+echo $"# generating thumbnails..." >&3
 montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/$name*.$format -caption '%f\n%wx%h' -font 'Liberation Sans' -pointsize 20 -background transparent -frame 5 ./done/Thumbnails.$format
-echo "# Das Programm ist fertig." >&3
-notify-send --app-name=imagemagick --icon=imagemagick -t 5000 Imagemagick "Konvertierung abgeschlossen" #im Paket libnotify-bin
+echo $"# conversion is ready." >&3
+notify-send --app-name=imagemagick --icon=imagemagick -t 5000 Imagemagick $"conversion is ready" #im Paket libnotify-bin
 rm ./done/logo.png
