f410357af1ea239f94ac88f3cc7ae259b58e4d33
Findus23 <l.winkler23@me.com>
2015-01-04 10:57:37 +0100

    Merge-Fehler ausgebessert

--- script.sh
+++ script.sh
@@ -8,8 +8,6 @@ else
 	rm ./done/*
 	echo "alte Dateien gelöscht"
 fi
-<<<<<<< HEAD
-
 format=$(zenity --entry --text "Welches Dateiformat soll ausgegeben werden?" --title "Dateiformat") || exit 1
 if [ -z $format ]
 then
@@ -22,20 +20,15 @@ then
 	name="2008­-Sanierung­-"
 fi
 
-=======
 echo "Logo wird verkleinert"
->>>>>>> ohne_bonus
 convert -monitor -resize 100x Logo-ohne-BRG-klein.png ./done/logo.png
 
 a=1
 for i in *.JPG
 do
-<<<<<<< HEAD
+
 	a_f="$(printf "%02d" $a)"
-	echo "./neu/Bild_$a.jpg"
-=======
 	echo "--------------./done/2008-­Sanierung-­$a.png------------------"
->>>>>>> ohne_bonus
 	convert -monitor -caption '%f' -resize 640x $i  \
 		-gravity SouthEast ./done/logo.png -geometry +15+15 \
 		-composite \
@@ -45,11 +38,6 @@ do
 		./done/$name­$a_f.$format
 	((a++))
 done
-<<<<<<< HEAD
-montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.$format -caption '%f\n%wx%h' -font 'Liberation Sans' -pointsize 20 -background transparent -frame 5 ./done/Thumbnails.$format
 rm ./done/logo.png
+montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.$format -caption '%f\n%wx%h' -font Liberation-Sans -pointsize 20 -background transparent -frame 5 ./done/Thumbnails.$format
 zenity --info --text "Das Programm ist fertig." --title "Fertig"
-=======
-rm ./done/logo.png
-montage -monitor  -label '%f\n%wx%h' -geometry '300x+5+5' ./done/*.png -caption '%f\n%wx%h' -font Liberation-Sans -pointsize 20 -background transparent -frame 5 ./done/thumbnails.PNG
->>>>>>> ohne_bonus
